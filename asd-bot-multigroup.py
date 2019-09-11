8ba36c7d2c0ac913e9f47cb8840b8b6b205c04bb
e-caste-mbp <en.cas.dev@icloud.com>
2019-09-11 23:53:38 +0200

    revert changes about Singleton pattern

--- asd-bot-multigroup.py
+++ asd-bot-multigroup.py
@@ -25,14 +25,14 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-# class Singleton(type):
-#     _instances = {}
-#     def __call__(cls, *args, **kwargs):
-#         if cls not in cls._instances:
-#             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-#         return cls._instances[cls]
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-class NotifiersManager():
+class NotifiersManager(metaclass=Singleton):
     def __init__(self, bot):
         self.notifiers = []
         self.bot = bot
