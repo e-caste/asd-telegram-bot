33fd1af5f444c9e37eafd2951a3bd6d220ae16d6
e-caste-mbp <en.cas.dev@icloud.com>
2019-09-26 11:52:04 +0200

    added many replies, reformatted for more readability

--- motivational_replies.py
+++ motivational_replies.py
@@ -3,23 +3,78 @@ ismore = [
     "\t'Ma il cielo è seeeeeempre più asd'",
     # "Team WEEE Open: da questa settimana con il 69% di asd in più!",
     "'L'asd scorre potente in lui...'",
+
     "'Asda tu che asdo anch'io,\n e questa settimana abbiamo asdato come non mai'",
+
     "'Asdame, asdame mucho\n"
     "como si fuera esta noche\n"
-    "lo ultimo asd...'"
+    "lo ultimo asd...'",
+
+    "Asd me back to the paradasd city,\nwhere the asd is green and the asds are pretty!",
+
+    "Na na na na na na na na na na na na na\nAsdman!",
+
+    "'Cause you're asd then you're cosd\n"
+    "You're yesd then you're nosd\n"
+    "You're isd then you're ousd\n"
+    "You're asd then you're dowsd",
+
+    "Alo?\nSalut!\nSunt eu, un haisduc",
+
+    "Never gonna asd you up\n"
+    "Never gonna asd you down\n"
+    "Never gonna asd around and desasd you",
+
+    "Now listen closely\n"
+    "Here's a little lesson in trisdery\n"
+    "This is going down in hisdory\n"
+    "If you wanna be a Villain Number Asd\n"
+    "You have to chase a superhero on the asd",
+
+    "Soy el fuego que arde tu asd\n"
+    "Soy el agua que asda tu sed\n"
+    "El castillo, la torre yo soy\n"
+    "La espada que asda el caudal.",
+
+    "What does the fox say?\n"
+    "Asdasdasdasdasdasd",
+
+    "Somebody once told me\n"
+    "The world was gonna asd me\n"
+    "I ain't the sharpest asd in the shed...",
+
+    "Asd Zwei Polizei\n"
+    "Drei Vier Grenadier",
+
+    "They see me asdin'...\nThey hatin'..."
 ]
 
 isless = [
     "Frateli e sorele, questa settimana avete avuto un leggero calo di asd, "
-    "ma vi sarà perdonato come a ogni buon WEEEstiano quando giungerà la sua ora.\n"
+    "ma vi sarà perdonato come a ogni buon ateo quando giungerà la sua ora.\n"
     "Asdate in pace",
+
     "'I find your lack of asd disturbing'\n"
     "\t- Master Asd in Asd Wars chapter 69: The return of the Asd",
+    "Where is my asd?\nWhere is my asd?",
+
+    "I tempi senza asd portano inevitabilmente a periodi con tanti asd, e tempi asdosi portano inevitabilmente a tempi "
+    "senza asd\n\t-Asdreulius",  # cit. koltissimah
+
+    "I bless the asds down in Africa...\nI bless the asds down in Africa...",
+
+    "If you like piña colasdas...\nAnd getting caught in the rain...",
+
+    "Lo sai che la tachipirina 500 se ne prendi due\nDiventa asd",
+
+    "I tried so hard and got so asd...\nBut in the end, it doesn't even matter..."
+
     # "Cari WEEEini, care WEEEine,\nState asdando troppo poco. Asdate di più.\n"
     # "Cordiali saluti,\nIl bot dell'asd"
 ]
 
 equals = [
     "Incredibile ma vero, avete asdato esattamente quanto la scorsa settimana!\n"
-    "Andate a festeggiare saldando un po' di condensatori!"
+    "C'era esattamente una probabilità su 69420, pazzesco!"
+    # "Andate a festeggiare saldando un po' di condensatori!"
 ]
